65fc8063ef6174754a705256e461458eefd615ad
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2022-12-12 17:30:51 +0530

    After complete UC11

--- SQLQuery2AddressBook.sql
+++ SQLQuery2AddressBook.sql
@@ -5,7 +5,7 @@ use AddressBook
 
 ------UC2------
 ----CREATING TABLE-----
-CREATE TABLE ADDRESS_BOOK_SERVICE
+CREATE TABLE ADDRESSBOOKSERVICE
 (
 FirstName char(25),
 LastName char(25),
@@ -25,3 +25,45 @@ INSERT INTO ADDRESS_BOOK_SERVICE VALUES('Sandesh','Nikam','Pune','Maharashtra','
 INSERT INTO ADDRESS_BOOK_SERVICE VALUES('rahul','saner','nashik','Maharashtra','574895','09384903','snaehal@gmail.com');
 INSERT INTO ADDRESS_BOOK_SERVICE VALUES('Priyanka','Ruhal','hyderabad','Telangana','455454','0938490334','priyanka@gmail.com');
 SELECT * FROM ADDRESS_BOOK_SERVICE;
+---UC4---------
+-----Edit Existing Contact-------
+update ADDRESS_BOOK_SERVICE set city ='VSP',Phone=9877161432,Email='Reventh143@gmail.com' ,StateName='Hyderabad',FirstName= 'Reventh'where LastName ='Nikam' ; 
+update ADDRESS_BOOK_SERVICE set FirstName ='Suresh',LastName='Prabu',StateName='Telengana',city='Hyd',Phone=9877161432,Email='Suresh143@gmail.com' where Zip=574895;  
+SELECT * FROM ADDRESS_BOOK_SERVICE;
+
+---UC5----
+----Delete a person-----
+Delete ADDRESS_BOOK_SERVICE where FirstName= 'Priyanka';
+SELECT * FROM ADDRESS_BOOK_SERVICE;
+
+------UC6------
+---Reterive the data using city or State---
+
+SELECT * FROM ADDRESS_BOOK_SERVICE where city='Visakhapatnam';
+SELECT * FROM ADDRESS_BOOK_SERVICE where StateName='Telengana';
+
+----UC7---
+----Size of ADB by City or state----
+select count(City) as City_count from ADDRESS_BOOK_SERVICE;
+select count(StateName) as State_count from ADDRESS_BOOK_SERVICE;
+SELECT * FROM ADDRESS_BOOK_SERVICE;
+------UC8-----
+----Sorte Alphabetically----
+SELECT * FROM ADDRESS_BOOK_SERVICE order by FirstName;
+SELECT * FROM ADDRESS_BOOK_SERVICE order by LastName;
+SELECT * FROM ADDRESS_BOOK_SERVICE;
+----UC9-Ability to identify each Address Book with name and Type----
+alter table ADDRESS_BOOK_SERVICE  add AddressBook_Name varchar(30),AddressBook_Type varchar(30);
+update ADDRESS_BOOK_SERVICE set AddressBook_Name = 'Family address book', AddressBook_Type = 'Family' where FirstName='Reventh';
+update ADDRESS_BOOK_SERVICE set AddressBook_Name = 'Family address book', AddressBook_Type = 'Friend' where FirstName='Suresh';
+
+select * from ADDRESS_BOOK_SERVICE;
+-----UC10-Ability to get number of contact persons using count typ----
+select count(AddressBook_Type) from ADDRESS_BOOK_SERVICE where AddressBook_Type = 'Friend';
+select * from ADDRESS_BOOK_SERVICE;
+
+----------UC11-Ability to add person to both Friend and Family---------------------
+
+INSERT INTO ADDRESS_BOOK_SERVICE VALUES('Eswar','Sgpv','Chennai','Tamilnadu','312425','70234651','eswar@gmail.com','Friends address book','Friends');
+INSERT INTO ADDRESS_BOOK_SERVICE VALUES('Ramana','Mamidi','karnataka','Bengalore','234523','9011596342','Ramana@gmail.com','Family address book','Family');
+select * from ADDRESS_BOOK_SERVICE;
